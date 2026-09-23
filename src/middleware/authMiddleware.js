7fda5f4f1448f90960dde54f75f926f807574375
Muhammad Abdurrahman <abdurrahmanjhelum@gmail.com>
2026-09-23 11:02:29 +0500

    Fix authMiddleware export to support both default and destructuring imports

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -26,3 +26,5 @@ const authMiddleware = async (req, res, next) => {
 };
 
 module.exports = authMiddleware;
+module.exports.protect = authMiddleware;
+module.exports.authMiddleware = authMiddleware;
